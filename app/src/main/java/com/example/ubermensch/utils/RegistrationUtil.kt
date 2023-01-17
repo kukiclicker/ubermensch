b3d35c3e0535b1e875f4a2ca474330e646f93994
Uros Mitrovic <umitrovic22@gmail.com>
2023-01-17 01:17:21 +0100

    Registration unit tests
    Registration unit tests added.

--- app/src/main/java/com/example/ubermensch/utils/RegistrationUtil.kt
+++ app/src/main/java/com/example/ubermensch/utils/RegistrationUtil.kt
@@ -1,11 +1,26 @@
 package com.example.ubermensch.utils
 
+import com.google.firebase.auth.FirebaseAuth
+
 object RegistrationUtil {
     fun userInputValidation(
         email:String,
         password:String,
         confirmedPassword:String
     ):Boolean{
+        if(email.isEmpty() || password.isEmpty())
+        {
+            return false
+        }
+        if(password != confirmedPassword)
+        {
+            return false
+        }
+        if(password.count{it.isDigit()}<2)
+        {
+            return false
+        }
+
         return true
     }
 }
--- app/src/test/java/com/example/ubermensch/utils/RegistrationUtilTest.kt
+++ app/src/test/java/com/example/ubermensch/utils/RegistrationUtilTest.kt
@@ -1,17 +1,63 @@
 package com.example.ubermensch.utils
 
-import org.junit.Assert.*
+
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
 class RegistrationUtilTest{
 
     @Test
-    fun testEmptyUsername(){
+    fun testEmptyEmail(){
         val result = RegistrationUtil.userInputValidation(
             "",
             "password",
             "password"
         )
-
+        assertThat(result).isFalse()
+    }
+    @Test
+    fun testValidEmail(){
+        val result = RegistrationUtil.userInputValidation(
+            "umitrovic22@gmail.com",
+            "password",
+            "password"
+        )
+        assertThat(result).isTrue()
+    }
+    @Test
+    fun testEmptyPassword(){
+        val result = RegistrationUtil.userInputValidation(
+            "name@gmail.com",
+            "",
+            "password"
+        )
+        assertThat(result).isFalse()
+    }
+    @Test
+    fun testConfirmPasswordCorrectly(){
+        val result = RegistrationUtil.userInputValidation(
+            "name@gmail.com",
+            "password",
+            "password"
+        )
+        assertThat(result).isFalse()
+    }
+    @Test
+    fun testConfirmPasswordIncorrectly(){
+        val result = RegistrationUtil.userInputValidation(
+            "name@gmail.com",
+            "password",
+            "password"
+        )
+        assertThat(result).isFalse()
+    }
+    @Test
+    fun testPasswordWithLessThen4Digits(){
+        val result = RegistrationUtil.userInputValidation(
+            "name@gmail.com",
+            "pas",
+            "pas"
+        )
+        assertThat(result).isFalse()
     }
 }
